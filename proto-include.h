d14f3c1862de6e6a7d498a4b11c27c5254315567
LightningMods <34066913+LightningMods@users.noreply.github.com>
2020-03-04 20:34:18 -0500

    Update proto-include.h

--- proto-include.h
+++ proto-include.h
@@ -76,7 +76,7 @@ typedef const FT_ServiceDescRec* FT_ServiceDesc;
 int FT_New_Memory_Face(FT_Library library, const FT_Byte* file_base, FT_Long file_size, FT_Long face_index, FT_Face* aface);
 int FT_Set_Char_Size(FT_Face  face, FT_F26Dot6  char_width, FT_F26Dot6 char_height, FT_UInt horz_resolution, FT_UInt  vert_resolution);
 int FT_Init_FreeType(FT_Library* alibrary);
-int FT_Load_Char(FT_Face face, FT_ULong  char_code, FT_Int32 load_flags);
+int FT_Load_Char(FT_Face face, FT_ULong  char_code, u32 load_flags);
 int FT_Done_FreeType(FT_Library  library);
 int FT_Done_Face(FT_Face  face);
 int FT_New_Face(FT_Library   library, const char* filepathname, FT_Long  face_index, FT_Face* aface);
@@ -96,7 +96,6 @@ int FT_Stroker_New(FT_Library  library, FT_Stroker* astroker);
 int FT_Stroker_Set(FT_Stroker stroker, FT_Fixed  radius, FT_Stroker_LineCap   line_cap, FT_Stroker_LineJoin  line_join, FT_Fixed  miter_limit);
 int FT_Open_Face(FT_Library library,const FT_Open_Args* args,FT_Long face_index,FT_Face* aface);
 signed long FT_MulFix(FT_Long a,FT_Long b);
-
 int FT_Select_Size(FT_Face face, FT_Int strike_index);
 int FT_Render_Glyph(FT_GlyphSlot slot,FT_Render_Mode render_mode);
 void FT_Outline_Transform(const FT_Outline* outline,const FT_Matrix* matrix);
